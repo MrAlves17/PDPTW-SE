b89e9b232e91b136e6bbf44a463969a7b6ce00d3
Vítor Alves Barbosa <38429470+MrAlves17@users.noreply.github.com>
2022-12-02 17:47:53 -0300

    print solution and removing new constraint

--- src/mip/modules/Formulations/src/Formulations.jl
+++ src/mip/modules/Formulations/src/Formulations.jl
@@ -9,6 +9,7 @@ using Infinity
 
 struct Solution
 	routes
+	times
 end
 
 export meloFormulation
@@ -61,18 +62,6 @@ function meloFormulation(inst::InstanceData, params::ParameterData)
 			@constraint(model, sum1 - sum2 == 0, base_name = "c2")
 		end
 	end
-
-	# cNovo
-	for k in inst.K
-		for i in inst.V[2:length(inst.V)]
-			for j in inst.V[2:length(inst.V)]
-				if (i,j) in inst.A
-					@constraint(model, x[i,j,k] + x[j,i,k] <= 1, base_name = "cNovo")
-				end
-			end
-		end
-	end
-
 
 	# c3
 	for k in inst.K
@@ -159,118 +148,118 @@ function meloFormulation(inst::InstanceData, params::ParameterData)
 
 	# # ### Scheduling constraints ###
 
-	# # c12
-	# for k in inst.K
-	# 	for (i,j) in inst.A
-	# 		if j in inst.V
-	# 			@constraint(model, t[j] >= t[i] + inst.s[i] + inst.d[i][j][k] - M*(1-x[i,j,k]), base_name = "c12")
-	# 		end
-	# 	end
-	# end
+	# c12
+	for k in inst.K
+		for (i,j) in inst.A
+			if j in inst.V
+				@constraint(model, t[j] >= t[i] + inst.s[i] + inst.d[i][j][k] - M*(1-x[i,j,k]), base_name = "c12")
+			end
+		end
+	end
 
-	# # c13
-	# for i in inst.V_p
-	# 	@constraint(model, t[i] + inst.s[i] <= t[inst.n+i], base_name="c13")
-	# end
+	# c13
+	for i in inst.V_p
+		@constraint(model, t[i] + inst.s[i] <= t[inst.n+i], base_name="c13")
+	end
 
-	# # c14
-	# for i in inst.V[2:length(inst.V)]
-	# 	@constraint(model, inst.tasks[inst.refs[i]].earl <= t[i] <= inst.tasks[inst.refs[i]].lat, base_name = "c14")
-	# end
+	# c14
+	for i in inst.V[2:length(inst.V)]
+		@constraint(model, inst.tasks[inst.refs[i]].earl <= t[i] <= inst.tasks[inst.refs[i]].lat, base_name = "c14")
+	end
 
-	# # c15
-	# fix(t[1], 0; force = true)
+	# c15
+	fix(t[1], 0; force = true)
 
-	# # # c16
-	# # for (i,j) in inst.A_m
-	# # 	sum1 = AffExpr(0)
-	# # 	for h in inst.H
-	# # 		add_to_expression!(sum1, phi[i,j,h])
-	# # 	end
+	# c16
+	for (i,j) in inst.A_m
+		sum1 = AffExpr(0)
+		for h in inst.H
+			add_to_expression!(sum1, phi[i,j,h])
+		end
 
-	# # 	sum2 = AffExpr(0)
-	# # 	for k in inst.K
-	# # 		add_to_expression!(sum2, x[i,j,k])
-	# # 	end
+		sum2 = AffExpr(0)
+		for k in inst.K
+			add_to_expression!(sum2, x[i,j,k])
+		end
 
-	# # 	@constraint(model, sum1 == sum2, base_name = "c16")
-	# # end
+		@constraint(model, sum1 == sum2, base_name = "c16")
+	end
 
-	# # c17
-	# for h in inst.H
-	# 	for k in inst.K
-	# 		for (i,j) in inst.A_m
-	# 			@constraint(model, alpha[i,j,h] >= t[i] + inst.s[i] + inst.d_bar[i][h][k] - M*(1-phi[i,j,h]), base_name = "c17")
-	# 		end
-	# 	end
-	# end
+	# c17
+	for h in inst.H
+		for k in inst.K
+			for (i,j) in inst.A_m
+				@constraint(model, alpha[i,j,h] >= t[i] + inst.s[i] + inst.d_bar[i][h][k] - M*(1-phi[i,j,h]), base_name = "c17")
+			end
+		end
+	end
 
-	# # # c18
-	# # for h in inst.H
-	# # 	for k in inst.K
-	# # 		for (i,j) in inst.A_m
-	# # 			if j in inst.V
-	# # 				@constraint(model, t[j] >= alpha[i,j,h] + inst.O[i][j][h] + inst.d_bar[j][h][k] - M*(2-phi[i,j,h]-x[i,j,k]), base_name = "c18")
-	# # 			end
-	# # 		end
-	# # 	end
-	# # end
+	# c18
+	for h in inst.H
+		for k in inst.K
+			for (i,j) in inst.A_m
+				if j in inst.V
+					@constraint(model, t[j] >= alpha[i,j,h] + inst.O[i][j][h] + inst.d_bar[j][h][k] - M*(2-phi[i,j,h]-x[i,j,k]), base_name = "c18")
+				end
+			end
+		end
+	end
 
-	# # c19
-	# for h in inst.H
-	# 	for (i,j) in inst.A_m
-	# 		for (iprime, jprime) in inst.A_m
-	# 			@constraint(model, gamma[i,j,iprime,jprime,h] + gamma[iprime,jprime,i,j,h] >= phi[i,j,h] + phi[iprime, jprime, h] - 1, base_name = "c19")
-	# 		end
-	# 	end
-	# end
+	# c19
+	for h in inst.H
+		for (i,j) in inst.A_m
+			for (iprime, jprime) in inst.A_m
+				@constraint(model, gamma[i,j,iprime,jprime,h] + gamma[iprime,jprime,i,j,h] >= phi[i,j,h] + phi[iprime, jprime, h] - 1, base_name = "c19")
+			end
+		end
+	end
 
-	# # c20
-	# for h in inst.H
-	# 	for (i,j) in inst.A_m
-	# 		for (iprime, jprime) in inst.A_m
-	# 			@constraint(model, gamma[i,j,iprime,jprime,h] <= phi[i,j,h], base_name = "c20")
-	# 		end
-	# 	end
-	# end
+	# c20
+	for h in inst.H
+		for (i,j) in inst.A_m
+			for (iprime, jprime) in inst.A_m
+				@constraint(model, gamma[i,j,iprime,jprime,h] <= phi[i,j,h], base_name = "c20")
+			end
+		end
+	end
 
-	# # c21
-	# for h in inst.H
-	# 	for (i,j) in inst.A_m
-	# 		for (iprime, jprime) in inst.A_m
-	# 			@constraint(model, gamma[iprime,jprime,i,j,h] <= phi[i,j,h], base_name = "c21")
-	# 		end
-	# 	end
-	# end
+	# c21
+	for h in inst.H
+		for (i,j) in inst.A_m
+			for (iprime, jprime) in inst.A_m
+				@constraint(model, gamma[iprime,jprime,i,j,h] <= phi[i,j,h], base_name = "c21")
+			end
+		end
+	end
 
-	# # # c22
-	# # for h in inst.H
-	# # 	for (i,j) in inst.A_m
-	# # 		for (iprime, jprime) in inst.A_m
-	# # 			@constraint(model, alpha[iprime,jprime,h] >= alpha[i,j,h] + inst.O[i][j][h] + inst.O[j][iprime][h] - M*(1-gamma[i,j,iprime,jprime,h]), base_name = "c22")
-	# # 		end
-	# # 	end
-	# # end
+	# c22
+	for h in inst.H
+		for (i,j) in inst.A_m
+			for (iprime, jprime) in inst.A_m
+				@constraint(model, alpha[iprime,jprime,h] >= alpha[i,j,h] + inst.O[i][j][h] + inst.O[j][iprime][h] - M*(1-gamma[i,j,iprime,jprime,h]), base_name = "c22")
+			end
+		end
+	end
 
-	# # # c23
-	# # for k in inst.K
-	# # 	for (i,p) in inst.A
-	# # 		if p == 2*inst.n+2
-	# # 			@constraint(model, C[k] >= t[i] + inst.s[i] + inst.d[i][2*inst.n+2][k] - M*(1-x[i,2*inst.n+2,k]), base_name = "c23")
-	# # 		end
-	# # 	end
-	# # end
+	# c23
+	for k in inst.K
+		for (i,p) in inst.A
+			if p == 2*inst.n+2
+				@constraint(model, C[k] >= t[i] + inst.s[i] + inst.d[i][2*inst.n+2][k] - M*(1-x[i,2*inst.n+2,k]), base_name = "c23")
+			end
+		end
+	end
 
-	# # # c24
-	# # for h in inst.H 
-	# # 	for k in inst.K
-	# # 		for (i,p) in inst.A_m
-	# # 			if p == 2*inst.n+2
-	# # 				@constraint(model, C[k] >= alpha[i,2*inst.n+2,h] + inst.O[i][2*inst.n+2][h] + inst.d_bar[2*inst.n+2][h][k] - M*(1-x[i,2*inst.n+2,k]), base_name = "c24")
-	# # 			end
-	# # 		end
-	# # 	end
-	# # end
+	# c24
+	for h in inst.H 
+		for k in inst.K
+			for (i,p) in inst.A_m
+				if p == 2*inst.n+2
+					@constraint(model, C[k] >= alpha[i,2*inst.n+2,h] + inst.O[i][2*inst.n+2][h] + inst.d_bar[2*inst.n+2][h][k] - M*(1-x[i,2*inst.n+2,k]), base_name = "c24")
+				end
+			end
+		end
+	end
 
 
 	# ### Objective function ###
@@ -306,9 +295,14 @@ function meloFormulation(inst::InstanceData, params::ParameterData)
 
 	x = value.(x)
 	z = value.(z)
+	t = value.(t)
+	C = value.(C)
+	phi = value.(phi)
+	gamma = value.(gamma)
+	alpha = value.(alpha)
 
-	sol = createSolutionMelo(inst,x,z)
-	# printMurrayFormulationSolution(inst,sol)
+	sol = createSolutionMelo(inst,x,z,t,C,phi,gamma,alpha)
+	printMeloFormulationSolution(inst,sol)
 
 	# if validateSolution(inst, sol)
 	# 	println("Everything is awesome!")
@@ -316,194 +310,197 @@ function meloFormulation(inst::InstanceData, params::ParameterData)
 
 end #function meloFormulation()
 
-function createSolutionMelo(inst::InstanceData, x, z)
+function createSolutionMelo(inst::InstanceData, x, z, t, C, phi, gamma, alpha)
 	M = 999999
 
 	# c1
-	for k in inst.K
-		sumX = 0
-		get_j = 0
-		for j in inst.V_p
-			sumX += x[1,j,k]
-			if x[1,j,k] == 1
-				get_j = j
-			end
-		end
-		sumX += x[1,2*inst.n+2,k]
-		if x[1,2*inst.n+2,k] == 1
-			get_j = 2*inst.n+2
-		end
-		# println(k)
-		println(sumX == 1)
-		println(1,' ',get_j,' ',k)
-		println(x[1,get_j,k])
-		# @constraint(model, sumX == 1, base_name = "c1")
-	end
-	println()
-	# c2
-	for k in inst.K
-		for i in inst.V[2:length(inst.V)]
-			sum1 = 0
-			get_j = 0
-			for (j,p) in inst.A
-				if p == i
-					sum1 += x[j,i,k]
-					if x[j,i,k] == 1
-						get_j = j
-					end
-				end
-			end
+	# for k in inst.K
+	# 	sumX = 0
+	# 	get_j = 0
+	# 	for j in inst.V_p
+	# 		sumX += x[1,j,k]
+	# 		if x[1,j,k] == 1
+	# 			get_j = j
+	# 		end
+	# 	end
+	# 	sumX += x[1,2*inst.n+2,k]
+	# 	if x[1,2*inst.n+2,k] == 1
+	# 		get_j = 2*inst.n+2
+	# 	end
+	# 	# println(k)
+	# 	println(sumX == 1)
+	# 	println(1,' ',get_j,' ',k)
+	# 	println(x[1,get_j,k])
+	# 	# @constraint(model, sumX == 1, base_name = "c1")
+	# end
+	# println()
+	# # c2
+	# for k in inst.K
+	# 	for i in inst.V[2:length(inst.V)]
+	# 		sum1 = 0
+	# 		get_j = 0
+	# 		for (j,p) in inst.A
+	# 			if p == i
+	# 				sum1 += x[j,i,k]
+	# 				if x[j,i,k] == 1
+	# 					get_j = j
+	# 				end
+	# 			end
+	# 		end
 
-			if sum1 > 0
-				println(get_j, i, k)
-				println(x[get_j,i,k])
-			end
-			println(sum1)
-			sum2 = 0
-			get_j = 0
-			for (p,j) in inst.A
-				if p == i
-					sum2 += x[i,j,k]
-					if x[i,j,k] == 1
-						get_j = j
-					end
-				end
-			end
+	# 		if sum1 > 0
+	# 			println(get_j, i, k)
+	# 			println(x[get_j,i,k])
+	# 		end
+	# 		println(sum1)
+	# 		sum2 = 0
+	# 		get_j = 0
+	# 		for (p,j) in inst.A
+	# 			if p == i
+	# 				sum2 += x[i,j,k]
+	# 				if x[i,j,k] == 1
+	# 					get_j = j
+	# 				end
+	# 			end
+	# 		end
 
-			if sum2 > 0
-				println(i, get_j, k)
-				println(x[i,get_j,k])
-			end
-			println(sum2)
-			println(sum1 - sum2 == 0)
+	# 		if sum2 > 0
+	# 			println(i, get_j, k)
+	# 			println(x[i,get_j,k])
+	# 		end
+	# 		println(sum2)
+	# 		println(sum1 - sum2 == 0)
 
-			# @constraint(model, sum1 - sum2 == 0, base_name = "c2")
-		end
-	end
-	println()
+	# 		# @constraint(model, sum1 - sum2 == 0, base_name = "c2")
+	# 	end
+	# end
+	# println()
 
-	# c3
-	for k in inst.K
-		sumX = 0
-		get_j = 0
-		for j in inst.V_d
-			sumX += x[j,2*inst.n+2,k]
-			if x[j,2*inst.n+2,k] == 1
-				get_j = j
-			end
-		end
-		sumX += x[1,2*inst.n+2, k]
-		if x[1,2*inst.n+2,k] == 1
-			get_j = 1
-		end
-		println(get_j, 2*inst.n+2, k)
-		println(x[get_j, 2*inst.n+2, k])
-		println(sumX)
-		println(sumX == 1)
-		# @constraint(model, sumX == 1, base_name = "c3")
-	end
+	# # c3
+	# for k in inst.K
+	# 	sumX = 0
+	# 	get_j = 0
+	# 	for j in inst.V_d
+	# 		sumX += x[j,2*inst.n+2,k]
+	# 		if x[j,2*inst.n+2,k] == 1
+	# 			get_j = j
+	# 		end
+	# 	end
+	# 	sumX += x[1,2*inst.n+2, k]
+	# 	if x[1,2*inst.n+2,k] == 1
+	# 		get_j = 1
+	# 	end
+	# 	println(get_j, 2*inst.n+2, k)
+	# 	println(x[get_j, 2*inst.n+2, k])
+	# 	println(sumX)
+	# 	println(sumX == 1)
+	# 	# @constraint(model, sumX == 1, base_name = "c3")
+	# end
 
-	println()
-	# c4
-	for i in inst.V[2:length(inst.V)]
-		sumX = 0
-		get_j = 0
-		get_k = 0
-		for k in inst.K
-			for (j,p) in inst.A
-				if p == i
-					sumX += x[j,i,k]
-					if x[j,i,k] == 1
-						get_j = j
-						get_k = k
-					end
-				end
-			end
-		end
-		println(get_j, i, get_k)
-		println(x[get_j,i,get_k])
-		println(sumX)
-		println(sumX == 1)
-		# @constraint(model, sumX == 1, base_name="c4")
-	end
+	# println()
+	# # c4
+	# for i in inst.V[2:length(inst.V)]
+	# 	sumX = 0
+	# 	get_j = 0
+	# 	get_k = 0
+	# 	for k in inst.K
+	# 		for (j,p) in inst.A
+	# 			if p == i
+	# 				sumX += x[j,i,k]
+	# 				if x[j,i,k] == 1
+	# 					get_j = j
+	# 					get_k = k
+	# 				end
+	# 			end
+	# 		end
+	# 	end
+	# 	println(get_j, i, get_k)
+	# 	println(x[get_j,i,get_k])
+	# 	println(sumX)
+	# 	println(sumX == 1)
+	# 	# @constraint(model, sumX == 1, base_name="c4")
+	# end
 
-	println()
-	# c5
-	for k in inst.K
-		for i in inst.V_p
-			sum1 = 0
-			get_j = 0
-			for (j,p) in inst.A
-				if p == i
-					sum1 += x[j,i,k]
-					if x[j,i,k] == 1
-						get_j = j
-					end
-				end
-			end
+	# println()
+	# # c5
+	# for k in inst.K
+	# 	for i in inst.V_p
+	# 		sum1 = 0
+	# 		get_j = 0
+	# 		for (j,p) in inst.A
+	# 			if p == i
+	# 				sum1 += x[j,i,k]
+	# 				if x[j,i,k] == 1
+	# 					get_j = j
+	# 				end
+	# 			end
+	# 		end
 
-			if sum1 > 0
-				println(get_j, i, k)
-				println(x[get_j,i,k])
-			end
-			println(sum1)
+	# 		if sum1 > 0
+	# 			println(get_j, i, k)
+	# 			println(x[get_j,i,k])
+	# 		end
+	# 		println(sum1)
 
-			sum2 = 0
-			get_j = 0
-			for (j,p) in inst.A
-				if p == inst.n+i
-					sum2 += x[j,inst.n+i,k]
-					if x[j,inst.n+i,k] == 1
-						get_j = j
-					end
-				end
-			end
+	# 		sum2 = 0
+	# 		get_j = 0
+	# 		for (j,p) in inst.A
+	# 			if p == inst.n+i
+	# 				sum2 += x[j,inst.n+i,k]
+	# 				if x[j,inst.n+i,k] == 1
+	# 					get_j = j
+	# 				end
+	# 			end
+	# 		end
 
-			if sum2 > 0
-				println(get_j, i+inst.n, k)
-				println(x[get_j,inst.n+i,k])
-			end
-			println(sum2)
+	# 		if sum2 > 0
+	# 			println(get_j, i+inst.n, k)
+	# 			println(x[get_j,inst.n+i,k])
+	# 		end
+	# 		println(sum2)
 
-			println(sum1 == sum2)
+	# 		println(sum1 == sum2)
 
-			# @constraint(model, sum1 == sum2, base_name="c5")
-		end
-	end
-	println()
+	# 		# @constraint(model, sum1 == sum2, base_name="c5")
+	# 	end
+	# end
+	# println()
 
 	routes = Any[]
+	times = Any[]
 	# println(x)
 
 	for k in inst.K
 		push!(routes, Any[])
-		if k == 1
-			continue
-		end
+		push!(times, Any[])
 		i=1
 		while i != length(inst.Vprime)
-			push!(routes[k], i-1)
+			push!(routes[k], inst.tasks[inst.refs[i]].id)
+			push!(times[k], t[i])
 			for j in inst.Vprime
-				# if i != j && x[i,j,k] > 0
-				# 	println(i,' ', j, ' ', k)
-				# 	i = j
-				# end
-				if i != j && (i,j) in inst.A && x[i,j,k] > 0
+				if (i,j) in inst.A && x[i,j,k] > 0
 					i = j
 					break
 				end
 			end
-			println(i)
-			break
 		end
-		push!(routes[k], i-1)
-		break
+		push!(routes[k], inst.tasks[inst.refs[i]].id)
+		push!(times[k], C[k])
 	end
 
-	println(routes)
-	sol = Solution(routes)
+	sol = Solution(routes, times)
 
 	return sol
 
 end # function createSolutionMelo()
+
+function printMeloFormulationSolution(inst::InstanceData, sol::Solution)
+	for k in inst.K
+		for i in 1:length(sol.routes[k])
+			print(instsol.routes[k][i], '(', sol.times[k][i], ')', " --> ")
+		end
+		println()
+	end
+
+end # function printMeloFormulationSolution
 end # module
